eb75203764ef7942f06d06f95f06b000c3bf88c2
anailson <arsantos315@gmail.com>
2025-10-18 19:17:55 -0300

    feat: teste da controller

--- src/main/java/com/ribeiro/transito/TransitoApiApplication.java
+++ src/main/java/com/ribeiro/transito/TransitoApiApplication.java
@@ -3,7 +3,7 @@ package com.ribeiro.transito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ribeiro")
 public class TransitoApiApplication {
 
 	public static void main(String[] args) {
