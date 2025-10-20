edf1e95c3f6beb98d058f0e22276922bdc1ef423
anailson <arsantos315@gmail.com>
2025-10-20 18:05:27 -0300

    feat-configurando e usando o Lompk

--- src/main/java/com/ribeiro/domain/model/Proprietario.java
+++ src/main/java/com/ribeiro/domain/model/Proprietario.java
@@ -1,5 +1,11 @@
 package com.ribeiro.domain.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Proprietario {
 
     private Long id;
@@ -7,35 +13,4 @@ public class Proprietario {
     private String email;
     private String telefone;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 }
