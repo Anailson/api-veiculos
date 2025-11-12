efc6a21d0dad5567463c3417ad16b3d365b2e01e
anailson <arsantos315@gmail.com>
2025-11-12 20:23:47 -0300

    Injetando e usando o repositório

--- src/main/java/com/ribeiro/controller/ProprietarioController.java
+++ src/main/java/com/ribeiro/controller/ProprietarioController.java
@@ -1,22 +1,25 @@
 package com.ribeiro.controller;
 
 import com.ribeiro.domain.model.Proprietario;
+import com.ribeiro.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+    private final ProprietarioRepository proprietarioRepository;
 
     @GetMapping("proprietarios")
     public List<Proprietario> listar() {
-              return manager.createQuery("from Proprietario", Proprietario.class ).getResultList();
+        return proprietarioRepository.findAll();
     }
 
 }
--- src/main/java/com/ribeiro/transito/TransitoApiApplication.java
+++ src/main/java/com/ribeiro/transito/TransitoApiApplication.java
@@ -3,9 +3,11 @@ package com.ribeiro.transito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.ribeiro")
-@EntityScan("com.ribeiro.domain.model") // força o scan das entidades
+@EntityScan("com.ribeiro.domain.model")
+@EnableJpaRepositories(basePackages = "com.ribeiro.domain.repository")
 public class TransitoApiApplication {
 
 	public static void main(String[] args) {
