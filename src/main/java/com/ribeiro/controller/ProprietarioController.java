77c65f0bb04d7dd7759140cdba14509c0a3aa1ed
anailson <arsantos315@gmail.com>
2025-11-15 11:03:37 -0300

    Implementando endpoint de inclusão de recurso

--- src/main/java/com/ribeiro/controller/ProprietarioController.java
+++ src/main/java/com/ribeiro/controller/ProprietarioController.java
@@ -3,11 +3,9 @@ package com.ribeiro.controller;
 import com.ribeiro.domain.model.Proprietario;
 import com.ribeiro.domain.repository.ProprietarioRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,13 @@ public class ProprietarioController {
                 .orElse(ResponseEntity.notFound().build());
 
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Proprietario adicionar(@RequestBody Proprietario proprietario){
+        return proprietarioRepository.save(proprietario);
+
+    }
+
 
 }
