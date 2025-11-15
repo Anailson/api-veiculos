53657122685a43d748946464fab9e8a40328f287
anailson <arsantos315@gmail.com>
2025-11-15 10:51:54 -0300

    Implementando endpoint de busca de recurso

--- src/main/java/com/ribeiro/controller/ProprietarioController.java
+++ src/main/java/com/ribeiro/controller/ProprietarioController.java
@@ -2,26 +2,33 @@ package com.ribeiro.controller;
 
 import com.ribeiro.domain.model.Proprietario;
 import com.ribeiro.domain.repository.ProprietarioRepository;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/proprietarios")
 public class ProprietarioController {
 
     private final ProprietarioRepository proprietarioRepository;
 
-    @GetMapping("proprietarios")
+    @GetMapping
     public List<Proprietario> listar() {
-       // return proprietarioRepository.findByNomeContaining("so");
-       // return proprietarioRepository.findByNome("anailson");
-             return    proprietarioRepository.findAll();
+        return proprietarioRepository.findAll();
+    }
+
+    @GetMapping("/{proprietarioId}")
+    public ResponseEntity<Proprietario> buscar(@PathVariable Long proprietarioId) {
+        return proprietarioRepository.findById(proprietarioId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+
     }
 
 }
--- src/main/java/com/ribeiro/domain/repository/ProprietarioRepository.java
+++ src/main/java/com/ribeiro/domain/repository/ProprietarioRepository.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
 @Repository
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
 
     //List<Proprietario> findByNome(String nome);
     //pesquisa por like
     List<Proprietario> findByNomeContaining(String nome);
+
 
 }
