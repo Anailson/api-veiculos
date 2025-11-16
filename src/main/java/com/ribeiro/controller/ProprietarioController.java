bdb3012e9f82535fb9f251931679582aa6a44bc3
anailson <arsantos315@gmail.com>
2025-11-16 18:19:25 -0300

    Implementando endpoint de exclusão de recurso

--- src/main/java/com/ribeiro/controller/ProprietarioController.java
+++ src/main/java/com/ribeiro/controller/ProprietarioController.java
@@ -31,21 +31,33 @@ public class ProprietarioController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Proprietario adicionar(@RequestBody Proprietario proprietario){
+    public Proprietario adicionar(@RequestBody Proprietario proprietario) {
         return proprietarioRepository.save(proprietario);
 
     }
 
     @PutMapping("/{proprietarioId}")
-    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario){
+    public ResponseEntity<Proprietario> atualizar(@PathVariable Long proprietarioId, @RequestBody Proprietario proprietario) {
 
-        if(!proprietarioRepository.existsById(proprietarioId)){
+        if (!proprietarioRepository.existsById(proprietarioId)) {
             return ResponseEntity.notFound().build();
         }
         proprietario.setId(proprietarioId);
         Proprietario proprietarioAtualizado = proprietarioRepository.save(proprietario);
 
-        return  ResponseEntity.ok(proprietarioAtualizado);
+        return ResponseEntity.ok(proprietarioAtualizado);
+
+    }
+
+    @DeleteMapping("/{proprietarioId}")
+    public ResponseEntity<Void> remover(@PathVariable Long proprietarioId) {
+
+        if (!proprietarioRepository.existsById(proprietarioId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        proprietarioRepository.deleteById(proprietarioId);
+        return ResponseEntity.noContent().build();
 
     }
 
