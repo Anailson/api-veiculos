f9eccab8420cdfb6fe745cb4f75e69e444173254
anailson <arsantos315@gmail.com>
2025-11-23 18:22:36 -0300

    Customizando as informações do ProblemDetail

--- src/main/java/com/ribeiro/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/ribeiro/exceptionhandler/ApiExceptionHandler.java
@@ -1,13 +1,34 @@
 package com.ribeiro.exceptionhandler;
 
 import com.ribeiro.domain.exception.NegocioException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
 
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        //caso tenha algum dominio que possa identificar o erro. Caso seja uma API externa para facilitar o entendimento
+        problemDetail.setType(URI.create("https://algatransito.com/erros/campos-invalidos"));
+
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
+
 
     @ExceptionHandler(NegocioException.class)
     public ResponseEntity<String> capturar(NegocioException e) {
