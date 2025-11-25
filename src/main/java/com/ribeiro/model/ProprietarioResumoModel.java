32027d828d700afc04a28e109e35e12a9594a33b
anailson <arsantos315@gmail.com>
2025-11-24 21:27:06 -0300

    Compondo objetos no Representation Model

---
+++ src/main/java/com/ribeiro/model/ProprietarioResumoModel.java
@@ -0,0 +1,12 @@
+package com.ribeiro.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProprietarioResumoModel {
+
+    private Long id;
+    private String nome;
+}
