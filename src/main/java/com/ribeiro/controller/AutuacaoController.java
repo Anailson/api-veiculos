3e88740d8984d03d8f3dc6984cdd570382efb9f4
anailson <arsantos315@gmail.com>
2025-12-02 19:05:36 -0300

    Implementando recurso de coleção de autuações

--- src/main/java/com/ribeiro/controller/AutuacaoController.java
+++ src/main/java/com/ribeiro/controller/AutuacaoController.java
@@ -2,13 +2,17 @@ package com.ribeiro.controller;
 
 import com.ribeiro.assembler.AutuacaoAssembler;
 import com.ribeiro.domain.model.Autuacao;
+import com.ribeiro.domain.model.Veiculo;
 import com.ribeiro.domain.model.input.AutuacaoInput;
 import com.ribeiro.domain.service.RegistroAutuacaoService;
+import com.ribeiro.domain.service.RegistroVeiculoService;
 import com.ribeiro.model.AutuacaoModel;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -17,6 +21,7 @@ public class AutuacaoController {
 
     private final AutuacaoAssembler autuacaoAssembler;
     private final RegistroAutuacaoService registroAutuacaoService;
+    private final RegistroVeiculoService registroVeiculoService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,5 +31,12 @@ public class AutuacaoController {
         Autuacao autuacaoRegistrada = registroAutuacaoService
                 .registrar(veiculoId, novaAutuacao);
         return autuacaoAssembler.toModel(autuacaoRegistrada);
+    }
+
+
+    @GetMapping
+    public List<AutuacaoModel> listar(@PathVariable Long veiculoId) {
+        Veiculo veiculo = registroVeiculoService.buscar(veiculoId);
+        return autuacaoAssembler.toCollectionModel(veiculo.getAutuacoes());
     }
 }
